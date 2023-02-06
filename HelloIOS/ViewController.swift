0574b0f58af5a5895462a2d323a21109739d2c1c
CoryLHarris <sirrahyroc@gmail.com>
2023-02-05 23:59:49 -0600

    IOS Initial Push

--- HelloIOS/ViewController.swift
+++ HelloIOS/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var morepetsSwtich: UISwitch!
+    @IBOutlet weak var numPetsStepper: UIStepper!
+    @IBOutlet weak var numPetsCount: UILabel!
+    @IBOutlet weak var SchoolTextField: UITextField!
+    @IBOutlet weak var LastNameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
